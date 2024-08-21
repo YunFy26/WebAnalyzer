c3481560366ce796a1f8e31a2671ec2b3d2eb228
YunFy26 <207946431@qq.com>
2024-08-21 13:49:11 +0800

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,6 @@ application {
 }
 
 val taieVersion = "0.5.1-SNAPSHOT" // or the latest version "0.5.1-SNAPSHOT"
-//val taieVersion = "0.2.2" // or the latest version "0.5.1-SNAPSHOT"
 
 dependencies {
     implementation("net.pascal-lab:tai-e:$taieVersion")
