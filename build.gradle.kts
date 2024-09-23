b1a35abce14808458ef5a3540f2ea082e9070842
YunFy26 <207946431@qq.com>
2024-09-23 13:07:10 +0800

    gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     application
+    id("com.github.johnrengelman.shadow") version "8.1.0"  // 版本根据需要调整
 }
 
 repositories {
@@ -26,9 +27,24 @@ application {
 
 val taieVersion = "0.5.1-SNAPSHOT" // or the latest version "0.5.1-SNAPSHOT"
 
+tasks {
+    jar {
+        manifest {
+            attributes(
+                "Main-Class" to "org.example.MyMain",  // 主类路径
+                "Tai-e-Version" to taieVersion,
+            )
+        }
+    }
+}
 dependencies {
     implementation("net.pascal-lab:tai-e:$taieVersion")
     implementation("org.apache.logging.log4j:log4j-api:2.23.0")
     implementation("org.apache.logging.log4j:log4j-core:2.23.0")
+    implementation("commons-cli:commons-cli:1.4")
+
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+
 
 }
