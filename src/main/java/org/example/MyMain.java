fb5ff4ffd87633179fa479d54704de75d3d0928f
YunFy26 <207946431@qq.com>
2024-09-23 13:05:51 +0800

    Main

--- src/main/java/org/example/MyMain.java
+++ src/main/java/org/example/MyMain.java
@@ -1,16 +1,44 @@
 package org.example;
 
 
+import org.apache.commons.cli.*;
+
+import java.text.ParseException;
+
 public class MyMain {
 
     public static void main(String[] args) {
+
+        Options options = new Options();
+
+        Option optionFile = new Option("o", "options-file", true, "配置文件路径");
+        optionFile.setRequired(true);
+        options.addOption(optionFile);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        String optionsFilePath;
+
+        try {
+            // 解析命令行参数
+            cmd = parser.parse(options, args);
+
+            // 获取 --options-file 参数的值
+            optionsFilePath = cmd.getOptionValue("options-file");
+
+        } catch (org.apache.commons.cli.ParseException e) {
+            System.out.println("错误: " + e.getMessage());
+            formatter.printHelp("--options-file=<path>", options);  // 打印帮助信息
+            System.exit(1);  // 退出程序
+            return;
+        }
+
         pascal.taie.Main.main(
                 "--options-file",
-                "configs/options.yml"
+                optionsFilePath
         );
-
-
-
     }
 
 
