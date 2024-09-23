5ac2f22ca288d192d250269733a57b9a71ee0155
YunFy26 <207946431@qq.com>
2024-09-23 13:05:25 +0800

    utils about spring

--- src/main/java/org/example/spring/SpringUtils.java
+++ src/main/java/org/example/spring/SpringUtils.java
@@ -1,6 +1,5 @@
 package org.example.spring;
 
-import pascal.taie.World;
 import pascal.taie.ir.stmt.LoadField;
 import pascal.taie.language.annotation.Annotation;
 import pascal.taie.language.classes.ClassHierarchy;
@@ -11,19 +10,65 @@ import pascal.taie.language.classes.JMethod;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 public class SpringUtils {
 
     private final ClassHierarchy hierarchy;
 
-    public SpringUtils(ClassHierarchy hierarchy) {
+    private final Set<BeanInfo> beanInfoSet;
+
+
+    public SpringUtils(ClassHierarchy hierarchy, Set<BeanInfo> beanInfoSet) {
         this.hierarchy = hierarchy;
+        this.beanInfoSet = beanInfoSet;
     }
 
+
     /**
-     * 判断一个字段是否是依赖注入的字段。
+     * 处理setter Injection
      */
-    public boolean isDependencyInjectionField(Collection<Annotation> annotations) {
+//    public boolean processSetterInjection(List<ControllerClass> controllerClasses){
+//
+//        for(ControllerClass controllerClass : controllerClasses){
+//            Collection<JMethod> methods = controllerClass.getJClass().getDeclaredMethods();
+//            for(JMethod method : methods){
+//                if(method.getName().startsWith("set")){
+//                    Collection<Annotation> annotations = method.getAnnotations();
+//                    if(hasDIAnnotation(annotations)){
+//                        Type paramType = method.getParamType(0);
+//                        System.out.println(paramType);
+//                        getImplClass(hierarchy.getClass(paramType.getName()), null);
+//                    }
+//                }
+//            }
+//        }
+//        return false;
+//    }
+//
+//
+//
+//    /**
+//     * 处理Constructor Injection
+//     */
+//    public boolean processConstructorInjection(List<ControllerClass> controllerClasses){
+//        for(ControllerClass controllerClass : controllerClasses){
+//            Collection<JMethod> methods = controllerClass.getJClass().getDeclaredMethods();
+//            for(JMethod method : methods){
+//                if(method.getName().equals(controllerClass.getJClass().getSimpleName())){
+//                    Collection<Annotation> annotations = method.getAnnotations();
+//                    return hasDIAnnotation(annotations);
+//                }
+//            }
+//        }
+//        return false;
+//    }
+
+
+    /**
+     * 判断是否有依赖注入的注解
+     */
+    private boolean hasDIAnnotation(Collection<Annotation> annotations) {
         for (Annotation annotation : annotations) {
             String annotationType = annotation.getType();
             if (annotationType.equals(DependencyInjectionType.Autowired.getType()) ||
@@ -33,6 +78,13 @@ public class SpringUtils {
             }
         }
         return false;
+    }
+
+    /**
+     * 判断一个字段是否是依赖注入的字段。
+     */
+    public boolean isDependencyInjectionField(JField field) {
+        return hasDIAnnotation(field.getAnnotations());
     }
 
     /**
@@ -92,8 +144,7 @@ public class SpringUtils {
         // 如果没有 @Qualifier 注解，则按照类型匹配
         Collection<JClass> subClasses = hierarchy.getAllSubclassesOf(fieldClass);
         List<JClass> concreteClasses = subClasses.stream()
-                .filter(jClass -> !jClass.isAbstract() && !jClass.isInterface() &&
-                        (jClass.hasAnnotation(ComponentType.Service.getType()) || jClass.hasAnnotation(ComponentType.Component.getType())))
+                .filter(jClass -> !jClass.isAbstract() && !jClass.isInterface() && isSpringBean(jClass))
                 .toList();
 
         if (concreteClasses.size() == 1) {
@@ -127,7 +178,7 @@ public class SpringUtils {
     /**
      * 判断一个类是否是 Spring Bean。
      */
-    public boolean isSpringBean(JClass jClass) {
+    public static boolean isSpringBean(JClass jClass) {
         return jClass.hasAnnotation(ComponentType.Component.getType()) ||
                 jClass.hasAnnotation(ComponentType.Service.getType()) ||
                 jClass.hasAnnotation(ComponentType.Repository.getType()) ||
@@ -137,17 +188,61 @@ public class SpringUtils {
     /**
      * 获取注解中的bean名称
      */
-    private String getComponentName(JClass jClass) {
+//    private String getComponentName(JClass jClass) {
+//        if (jClass.hasAnnotation(ComponentType.Service.getType()) &&
+//                Objects.requireNonNull(jClass.getAnnotation(ComponentType.Service.getType())).hasElement("value")) {
+//            return Objects.requireNonNull(Objects.requireNonNull(jClass.getAnnotation(ComponentType.Service.getType())).getElement("value")).toString();
+//        } else if (jClass.hasAnnotation(ComponentType.Component.getType()) &&
+//                Objects.requireNonNull(jClass.getAnnotation(ComponentType.Component.getType())).hasElement("value")) {
+//            return Objects.requireNonNull(Objects.requireNonNull(jClass.getAnnotation(ComponentType.Component.getType())).getElement("value")).toString();
+//        }
+//        // 默认使用类名首字母小写
+//        String className = jClass.getSimpleName();
+//        return className.substring(0, 1).toLowerCase() + className.substring(1);
+//    }
+    public String getComponentName(JClass jClass) {
+        // 检查是否有 @Service 注解并获取 value
         if (jClass.hasAnnotation(ComponentType.Service.getType()) &&
                 Objects.requireNonNull(jClass.getAnnotation(ComponentType.Service.getType())).hasElement("value")) {
-            return Objects.requireNonNull(Objects.requireNonNull(jClass.getAnnotation(ComponentType.Service.getType())).getElement("value")).toString();
-        } else if (jClass.hasAnnotation(ComponentType.Component.getType()) &&
-                Objects.requireNonNull(jClass.getAnnotation(ComponentType.Component.getType())).hasElement("value")) {
-            return Objects.requireNonNull(Objects.requireNonNull(jClass.getAnnotation(ComponentType.Component.getType())).getElement("value")).toString();
+            return Objects.requireNonNull(Objects.requireNonNull(jClass.getAnnotation(ComponentType.Service.getType()))
+                    .getElement("value")).toString();
         }
-        // 默认使用类名首字母小写
+
+        // 检查是否有 @Component 注解并获取 value
+        if (jClass.hasAnnotation(ComponentType.Component.getType()) &&
+                Objects.requireNonNull(jClass.getAnnotation(ComponentType.Component.getType())).hasElement("value")) {
+            return Objects.requireNonNull(Objects.requireNonNull(jClass.getAnnotation(ComponentType.Component.getType()))
+                    .getElement("value")).toString();
+        }
+
+        // 检查是否有 @Repository 注解并获取 value
+        if (jClass.hasAnnotation(ComponentType.Repository.getType()) &&
+                Objects.requireNonNull(jClass.getAnnotation(ComponentType.Repository.getType())).hasElement("value")) {
+            return Objects.requireNonNull(Objects.requireNonNull(jClass.getAnnotation(ComponentType.Repository.getType()))
+                    .getElement("value")).toString();
+        }
+
+        // 检查是否有 @Controller 注解并获取 value
+        if (jClass.hasAnnotation(ComponentType.Controller.getType()) &&
+                Objects.requireNonNull(jClass.getAnnotation(ComponentType.Controller.getType())).hasElement("value")) {
+            return Objects.requireNonNull(Objects.requireNonNull(jClass.getAnnotation(ComponentType.Controller.getType()))
+                    .getElement("value")).toString();
+        }
+
+        // 默认使用类名首字母小写作为组件名称
         String className = jClass.getSimpleName();
         return className.substring(0, 1).toLowerCase() + className.substring(1);
     }
+
+
+    public JClass searchBeanByDefaultName(String fieldName, BeanInfo beanInfo) {
+        return fieldName.equals(beanInfo.getDefaultName()) ? beanInfo.getBeanClass() : null;
+    }
+
+    public String processFieldInjection(JField field){
+        FieldOfFieldInjection fieldOfFieldInjection = new FieldOfFieldInjection(field);
+        return fieldOfFieldInjection.getSpecifyName();
+    }
+
 
 }
