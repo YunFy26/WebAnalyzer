66d6b264f867b17f23d236edff13fbce4854deea
YunFy26 <207946431@qq.com>
2024-10-19 21:03:17 +0800

    Injection Point

--- src/main/java/org/example/spring/InjectionPoint.java
+++ src/main/java/org/example/spring/InjectionPoint.java
@@ -5,9 +5,10 @@ import pascal.taie.language.classes.JField;
 import java.util.Objects;
 
 /**
- * 实体类
- * 对于FieldInjection这种注入方式（即直接在field上面写依赖注入相关的注解）实例化一个FieldOfFieldInjection类
- * 用于提供相关操作接口
+ * entity class
+ * for FieldInjection   make a InjectionPoint instance, represent an injection point
+ * provide some api
+ * TODO: for setter injection and constructor injection, provide api
  */
 public class InjectionPoint {
 
@@ -23,13 +24,13 @@ public class InjectionPoint {
     public String getSpecifyName() {
         // @Qualifier 和 @Resource 一般不同时出现，@Qualifier是辅助 @Autowired 和 @Inject 的
         // 获取 @Qualifier 指定的名称
-        if (field.hasAnnotation(DependencyInjectionType.Qualifier.getType())) {
-            return Objects.requireNonNull(Objects.requireNonNull(field.getAnnotation(DependencyInjectionType.Qualifier.getType()))
+        if (field.hasAnnotation(InjectionAnnotationRules.Qualifier.getType())) {
+            return Objects.requireNonNull(Objects.requireNonNull(field.getAnnotation(InjectionAnnotationRules.Qualifier.getType()))
                     .getElement("value")).toString();
         }
         // 获取 @Resource 指定的名称
-        if (field.hasAnnotation(DependencyInjectionType.Resource.getType())) {
-            return Objects.requireNonNull(Objects.requireNonNull(field.getAnnotation(DependencyInjectionType.Qualifier.getType()))
+        if (field.hasAnnotation(InjectionAnnotationRules.Resource.getType())) {
+            return Objects.requireNonNull(Objects.requireNonNull(field.getAnnotation(InjectionAnnotationRules.Qualifier.getType()))
                     .getElement("name")).toString();
         }
 
