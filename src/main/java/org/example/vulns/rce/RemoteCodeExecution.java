94ffa45a4d2df072f9804cf5088de799bbea8d38
Yuntsy <207946431@qq.com>
2024-11-14 12:46:31 +0800

    Rce

---
+++ src/main/java/org/example/vulns/rce/RemoteCodeExecution.java
@@ -0,0 +1,12 @@
+package org.example.vulns.rce;
+
+import org.example.vulns.AbstractVulnerabilityData;
+
+public class RemoteCodeExecution extends AbstractVulnerabilityData {
+
+    @Override
+    public String getName() {
+        return "RCE-RemoteCodeExecution";
+    }
+
+}
