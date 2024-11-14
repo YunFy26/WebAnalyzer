d4fce6849a055b8cbfdb169abfdb82587d6b2f20
Yuntsy <207946431@qq.com>
2024-11-14 12:46:51 +0800

    redirect

---
+++ src/main/java/org/example/vulns/redirect/OpenRedirect.java
@@ -0,0 +1,12 @@
+package org.example.vulns.redirect;
+
+import org.example.vulns.AbstractVulnerabilityData;
+
+public class OpenRedirect extends AbstractVulnerabilityData {
+
+    @Override
+    public String getName() {
+        return "OpenRedirect";
+    }
+
+}
