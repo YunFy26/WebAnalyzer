590f094bc1ca8759e6b99e5b809454c475c9fcf7
Yuntsy <207946431@qq.com>
2024-11-14 12:44:17 +0800

    AFO

---
+++ src/main/java/org/example/vulns/files/ArbitraryFileOverwrite.java
@@ -0,0 +1,12 @@
+package org.example.vulns.files;
+
+import org.example.vulns.AbstractVulnerabilityData;
+
+public class ArbitraryFileOverwrite extends AbstractVulnerabilityData {
+
+    @Override
+    public String getName() {
+        return "AFO-ArbitraryFileOverwrite";
+    }
+
+}
