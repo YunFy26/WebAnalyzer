8fcd2d801db90360e7dfd8ad6201c4fb4d63764b
Yuntsy <207946431@qq.com>
2024-11-14 12:45:33 +0800

    idor

---
+++ src/main/java/org/example/vulns/idor/InsecureDirectObjectReferences.java
@@ -0,0 +1,12 @@
+package org.example.vulns.idor;
+
+import org.example.vulns.AbstractVulnerabilityData;
+
+public class InsecureDirectObjectReferences extends AbstractVulnerabilityData {
+
+    @Override
+    public String getName() {
+        return "IDOR-InsecureDirectObjectReferences";
+    }
+
+}
