5112dd302de61c3e9adf276c70a75b94d25707db
Yuntsy <207946431@qq.com>
2024-11-14 12:44:31 +0800

    File Upload

---
+++ src/main/java/org/example/vulns/files/FileUpload.java
@@ -0,0 +1,13 @@
+package org.example.vulns.files;
+
+import org.example.vulns.AbstractVulnerabilityData;
+
+
+public class FileUpload extends AbstractVulnerabilityData {
+
+    @Override
+    public String getName() {
+        return "FileUpload";
+    }
+
+}
