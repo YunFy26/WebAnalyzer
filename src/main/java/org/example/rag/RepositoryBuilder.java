a338e235a3cf6ae4e433b237e1896714ad65333c
Yuntsy <207946431@qq.com>
2024-11-14 12:41:32 +0800

    build vulns reposity

--- src/main/java/org/example/rag/RepositoryBuilder.java
+++ src/main/java/org/example/rag/RepositoryBuilder.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.example.rag.es.ElasticsearchUtils;
 import org.example.rag.es.MethodData;
+import org.example.vulns.VulnerabilityLoader;
 import pascal.taie.analysis.graph.callgraph.CallGraph;
 import pascal.taie.ir.IR;
 import pascal.taie.ir.exp.Var;
@@ -16,28 +17,32 @@ import java.util.List;
 
 public class RepositoryBuilder {
 
-    private final Logger logger = LogManager.getLogger(RepositoryBuilder.class);
+    private static final Logger logger = LogManager.getLogger(RepositoryBuilder.class);
 
-    private final CallGraph<Invoke, JMethod> callGraph;
+    private static final VulnerabilityLoader loader = new VulnerabilityLoader("configs/vulns.json");
 
-    private final List<MethodData> methodDataList = new ArrayList<>();
 
-    public RepositoryBuilder(CallGraph<Invoke, JMethod> callGraph) {
-        this.callGraph = callGraph;
-    }
+//    private final CallGraph<Invoke, JMethod> callGraph;
 
-    private void setEntityList(){
-        callGraph.reachableMethods().forEach(jMethod -> {
-            String signature = jMethod.getSignature();
-            IR ir = jMethod.getIR();
-            List<Var> vars = ir.getVars();
-            List<Stmt> stmts = ir.getStmts();
-            methodDataList.add(new MethodData(signature, vars, stmts));
-        });
-    }
+//    private final List<MethodData> methodDataList = new ArrayList<>();
+
+//    public RepositoryBuilder(CallGraph<Invoke, JMethod> callGraph) {
+//        this.callGraph = callGraph;
+//    }
+
+//    private void setEntityList(){
+//        callGraph.reachableMethods().forEach(jMethod -> {
+//            String signature = jMethod.getSignature();
+//            IR ir = jMethod.getIR();
+//            List<Var> vars = ir.getVars();
+//            List<Stmt> stmts = ir.getStmts();
+//            methodDataList.add(new MethodData(signature, vars, stmts));
+//        });
+//    }
 
     public void build(){
-        setEntityList();
-        methodDataList.forEach(ElasticsearchUtils::storeData);
+//        setEntityList();
+//        methodDataList.forEach(ElasticsearchUtils::storeMethodData);
+        loader.getVulnerabilities().forEach(ElasticsearchUtils::storeVulnData);
     }
 }
