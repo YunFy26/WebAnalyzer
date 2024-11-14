b66ba01b9658bdbda534ea7811dc350b3d65ca1f
Yuntsy <207946431@qq.com>
2024-11-14 12:43:57 +0800

    Deserialization

---
+++ src/main/java/org/example/vulns/deserialization/Deserialization.java
@@ -0,0 +1,13 @@
+package org.example.vulns.deserialization;
+
+import org.example.vulns.AbstractVulnerabilityData;
+
+
+public class Deserialization extends AbstractVulnerabilityData {
+
+    @Override
+    public String getName() {
+        return "Deserialization";
+    }
+
+}
