7e7e24920a6c01d3fdf5fd9518bf3dfb42d07976
Yuntsy <207946431@qq.com>
2024-11-14 12:45:06 +0800

    Path Traversal

---
+++ src/main/java/org/example/vulns/files/PathTraversal.java
@@ -0,0 +1,12 @@
+package org.example.vulns.files;
+
+import org.example.vulns.AbstractVulnerabilityData;
+
+public class PathTraversal extends AbstractVulnerabilityData {
+
+    @Override
+    public String getName() {
+        return "PathTraversal";
+    }
+
+}
