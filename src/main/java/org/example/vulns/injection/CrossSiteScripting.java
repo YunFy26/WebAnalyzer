7b2a786f5f142f78478f24dbd19753ac10bed4b4
Yuntsy <207946431@qq.com>
2024-11-14 12:46:13 +0800

    xss

---
+++ src/main/java/org/example/vulns/injection/CrossSiteScripting.java
@@ -0,0 +1,12 @@
+package org.example.vulns.injection;
+
+import org.example.vulns.AbstractVulnerabilityData;
+
+public class CrossSiteScripting extends AbstractVulnerabilityData {
+
+    @Override
+    public String getName() {
+        return "XSS-CrossSiteScripting";
+    }
+
+}
